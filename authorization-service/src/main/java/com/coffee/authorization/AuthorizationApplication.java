bfc88cccdc0e57602b313be0af8c0e3b90bcbeb9
haivutuan93 <haivutuan93@gmail.com>
2023-09-19 00:13:10 +0700

    EnableScheduling EnableCaching for authorization service

--- authorization-service/src/main/java/com/coffee/authorization/AuthorizationApplication.java
+++ authorization-service/src/main/java/com/coffee/authorization/AuthorizationApplication.java
@@ -4,9 +4,13 @@ import com.coffee.common.SharedConfigurationReference;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Import;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Import({SharedConfigurationReference.class})
+@EnableScheduling
+@EnableCaching
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class AuthorizationApplication {
 
